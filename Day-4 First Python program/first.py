fa4e9840ccf4bd6997100bd27443344a4baeb55b
Ujjwal <ujjwalkamila86@gmail.com>
2023-12-13 19:02:25 +0530

    initial commit

---
+++ Day-4 First Python program/first.py
@@ -0,0 +1,14 @@
+# First Program
+
+print("Hello World")
+
+#This is a single line comment ...by using "ctrl+/"
+''''
+This 
+is 
+a 
+multiple 
+line
+comment'''
+
+print("Hey i am a \" good boy\"n and he is also a good boy")
