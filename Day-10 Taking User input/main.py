d321f86e9d2f65f135f5801821a4aaf7499197b4
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-15 19:39:12 +0530

    Take user input in python

---
+++ Day-10 Taking User input/main.py
@@ -0,0 +1,10 @@
+# Day 10 - Taking User Input in python
+# Syntax :  "variable=input()"
+a = input("Enter your name: ") #take input and prints
+print("My name is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x  + y) #print as str
+
+print(int(x) + int(y)) #print as int type
