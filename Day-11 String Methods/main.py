fde164940c52d12bcd285bb19cc020e33a2b5444
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-16 11:51:38 +0530

    take user input in python

---
+++ Day-11 String Methods/main.py
@@ -0,0 +1,23 @@
+name = "Ujjwal"
+friend = "Rahul"
+anotherFriend = 'Rudra'
+# Multiline Strings
+apple = '''He said, 
+Hi Ujjwal
+hey I am good
+"I want to eat an apple'''
+ 
+print("Hello, " + name)
+# print(apple) 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+# print(name[5]) # Throws an error 5-index is not here
+print("Lets use a for loop\n")
+
+# Looping through the string
+for character in apple: #For loop
+    print(character)
