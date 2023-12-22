4e4cf760fe2decf18a0399bb03fa3edb4e0c8e08
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-22 22:05:21 +0530

    Day-30 Recursion in Python

---
+++ Day-30 Recursion in Python/Day30Recursion.py
@@ -0,0 +1,50 @@
+# Recursion in python
+
+# factorial(7) = 7*6*5*4*3*2*1
+# factorial(6) = 6*5*4*3*2*1
+# factorial(5) = 5*4*3*2*1
+# factorial(4) = 4*3*2*1
+# factorial(0) = 1
+
+
+# factorial(n) = n * factorial(n-1)
+
+#Factorial function using recursion
+def factorial(n):
+  if (n == 0 or n == 1):
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+# num = 7; 
+# print("Number: ",num)
+# print("Factorial: ",factorial(num))
+
+
+print(factorial(5))
+print(factorial(4))
+print(factorial(3))
+# 5 * factorial(4)
+# 5 * 4 * factorial(3)
+# 5 * 4 * 3 * factorial(2)
+# 5 * 4 * 3 * 2 * factorial(1)
+# 5 * 4 * 3 * 2 * 1
+
+# Quick Quiz: Write a program to print the Fibonacci sequence
+# f(0) = 0
+# f(1) = 1
+# f(2) = f(1) + f(0)
+# f(n) = f(n-1) + f(n-2)
+
+# 0 1 1 2 3 5 8....
+
+#fibonacci sequence 
+
+def fib(n):
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fib(n-1) + fib(n-2)
+print(fib(6))
