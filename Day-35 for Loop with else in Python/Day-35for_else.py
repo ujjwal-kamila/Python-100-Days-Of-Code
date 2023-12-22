d9db6c93a535db18e7ecf6881d849dfbcb25d633
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-23 01:29:27 +0530

    Day-35 For loop with else statement

---
+++ Day-35 for Loop with else in Python/Day-35for_else.py
@@ -0,0 +1,24 @@
+# Day-35 For loop with else statement
+# for i in range(5):
+#     print(i)
+#     if i==4:
+#         break
+    
+# else:
+#     print("sorry no i ")
+
+# i = 0
+# while i<7:
+#     print(i)
+#     i+=1
+#     # if i==4:
+#     #     break
+    
+# else:
+#     print("sorry no i ")
+
+for x in range(5):
+    print ("iteration no {} in for loop".format(x+1))
+else:
+    print ("else block in loop")
+print ("Out of loop")
