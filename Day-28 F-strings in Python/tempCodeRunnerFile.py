7f7fa2229bc1968c6700ee5f75fc98ade985f940
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-22 21:55:25 +0530

    Day-28 F-strings in Python

---
+++ Day-28 F-strings in Python/tempCodeRunnerFile.py
@@ -0,0 +1,21 @@
+
+# # f-strings in python
+# letter = "Hello, My name is {1} and I  am from {0}" 
+# country = "India"
+# name = "Ujjwal"
+
+# print(letter.format(country,name))
+# print(f"Hello, My name is {name} and I  am from {country}")  #uses of fsrings
+
+# price = 49.4342009
+# txt = f"For only {price:.2f} dollars!"  #take upto which point  #uses of fsrings
+# print(txt)
+# print(f"type{2 * 10}")
+# print(type(f"{2 * 10}"))
+
+# # f-strings in python
+# val = 'Geeks'  
+# print(f"{val}for{val} is a portal for {val}.")  
+# name = 'Ujjwal'  
+# age = 19 
+# print(f"Hello, My name is {name} and I'm {age} years old.")
