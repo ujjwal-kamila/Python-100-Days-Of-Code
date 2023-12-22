6a119c4ce0a389b1288555e787d8dd5f70472f35
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-22 21:54:46 +0530

    Day28 F-strings in Python

---
+++ Day-28 F-strings in Python/Day28fstrings.py
@@ -0,0 +1,26 @@
+# String formatting in python
+txt = "For only {price:.2f} dollars!"   #.2f is float value upto like 1.00
+print(txt.format(price = 49))
+
+
+
+# f-strings in python
+letter = "Hello, My name is {1} and I  am from {0}" 
+country = "India"
+name = "Ujjwal"
+
+print(letter.format(country,name))
+print(f"Hello, My name is {name} and I  am from {country}")  #uses of fsrings
+
+price = 49.4342009
+txt = f"For only {price:.2f} dollars!"  #take upto which point  #uses of fsrings
+print(txt)
+print(f"type{2 * 10}")
+print(type(f"{2 * 10}"))
+
+# f-strings in python
+val = 'Geeks'  
+print(f"{val}for{val} is a portal for {val}.")  
+name = 'Ujjwal'  
+age = 19 
+print(f"Hello, My name is {name} and I'm {age} years old.")
