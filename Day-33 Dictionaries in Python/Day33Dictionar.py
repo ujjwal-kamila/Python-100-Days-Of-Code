f893a02e6599c96122c3818f3fede5820c0f4593
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-23 01:17:27 +0530

    Day-33 Dictionaries in Python

---
+++ Day-33 Dictionaries in Python/Day33Dictionar.py
@@ -0,0 +1,31 @@
+# Day-33 Dictionaries in Python
+
+
+# dic = {
+#     "Ujjwal" : "Human being ",
+#     "Book": "Object"
+# }
+# print(dic["Ujjwal"])
+
+info = {'name':'Ujjwal','age':19 ,'eligible':True}
+print(info)
+#Acessing single values 
+print(info['name'])
+print(info.get('eligible'))
+#Acessing Multiple values 
+print(info.values())
+
+
+for key in info.keys():
+  print(f"The value corresponding to the key {key} is {info[key]}")
+
+print(info.items())
+for key, value in info.items():
+  print(f"The value corresponding to the key {key} is {value}") 
+
+# #Acessing keys 
+# info = {'name':'Ujjwal', 'age':19, 'eligible':True}
+# print(info.keys())
+# #Accessing key-value pairs
+# info = {'name':'Ujjwal', 'age':19, 'eligible':True}
+# print(info.items())
