f251aa4b3e5a58b1fe70a413ac698bbcdf953705
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-22 22:00:39 +0530

    Day-29 Doc_strings in Python

---
+++ Day-29 Doc_strings in Python/Day29Docstrings.py
@@ -0,0 +1,8 @@
+# Day-29 Doc_strings in Python
+def square(n):
+  '''Takes in a number n, returns the square of n'''
+  print(n**2)
+square(5)
+print(square.__doc__)   #prints comments 
+
+import this
