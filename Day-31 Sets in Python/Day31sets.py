a8213824810b7229a7a1762f373d3fcfc85a7af2
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-22 22:13:58 +0530

    Updated day 31 and 32 Set & Set Methods

--- Day-31 Sets in Python/Day31sets.py
+++ Day-31 Sets in Python/Day31sets.py
@@ -1,7 +1,6 @@
 # Sets in Python
 
 # Set Methods
-
 s = {2,3,4,2}
 print(s)
 
@@ -9,12 +8,10 @@ info = {"Ujjwal",20, False,5.6,20}
 print(info)
 
 #Access by using for loop
-
 info = {"Carla", 19, False, 5.9}
 for item in info:
     print(item)
 
 # create empty set
-
 ujjwal=set()
 print(type(ujjwal))
