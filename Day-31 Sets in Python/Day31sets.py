6a9490ed34d9f85298d7deb463b7b5bfe6da1931
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-22 22:07:20 +0530

    Day-31 Sets in Python

---
+++ Day-31 Sets in Python/Day31sets.py
@@ -0,0 +1,20 @@
+# Sets in Python
+
+# Set Methods
+
+s = {2,3,4,2}
+print(s)
+
+info = {"Ujjwal",20, False,5.6,20}
+print(info)
+
+#Access by using for loop
+
+info = {"Carla", 19, False, 5.9}
+for item in info:
+    print(item)
+
+# create empty set
+
+ujjwal=set()
+print(type(ujjwal))
