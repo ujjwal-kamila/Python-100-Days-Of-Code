d98865471288458ba2e7bce8eb36508c338295c7
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-23 01:46:50 +0530

    Day-38 Custom Errors in Python

--- Day-38 Custom Errors in Python/Day-38custom_error.py
+++ Day-38 Custom Errors in Python/Day-38custom_error.py
@@ -1,4 +1,5 @@
 #Day-38 Custom Errors in Python
+
 # # Value errors 
 # a = int(input("Enter any value between 5 and 20 :"))
 
@@ -13,3 +14,19 @@ if(a == "quit"):
     print("quit")
 elif(a<5 or a>9):
     raise ValueError("Value should be between 5 and 9")
+
+#Raising Custom errors 
+salary = int(input("Enter salary amount: "))
+if not 2000 < salary < 5000:
+    raise ValueError("Not a valid salary")
+
+# Defining Custom Exceptions
+class CustomError(Exception):
+ ''' # code ...
+  pass
+
+try:
+  # code ...
+
+except CustomError:
+  # code...'''
