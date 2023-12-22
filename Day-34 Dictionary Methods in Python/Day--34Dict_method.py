b5d22901ed1feccfbfc028d5cdce3b8eefa26ff2
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-23 01:24:24 +0530

    Day-34 Dictionary Methods in Python

---
+++ Day-34 Dictionary Methods in Python/Day--34Dict_method.py
@@ -0,0 +1,10 @@
+#Dictionary methods
+ep1 = {122: 45, 123: 89, 567: 69, 670: 69}
+ep2 = {222: 67, 566: 90}
+
+# ep1.update(ep2)  # for update with another dict
+# ep1.clear()  #for clear the whole dict
+# ep1.pop(122) #delet the item 
+ep1.popitem()  #delet the last item 
+del ep1[122]
+print(ep1)
