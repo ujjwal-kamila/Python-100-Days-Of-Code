e7d638ce7fc7c84aaeecfb58b176fbe163861d5e
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-24 21:17:16 +0530

    Day-43 Virtual Enviroment in Python

---
+++ Day-43 Virtual Enviroment in Python/Day43virtual_env.py
@@ -0,0 +1,25 @@
+
+# Day-43 Virtual Enviroment in Python
+
+#Virtual envorment is used to isolate specific Pythhon enviorments in a sinle machine
+
+#create a folder "Python venv" and run the below commands
+''''
+# Create a virtual environment
+"python -m venv myenv"
+
+# Activate the virtual environment (Linux/macOS)
+"source myenv/bin/activate"
+
+# Activate the virtual environment (Windows)
+"myenv\Scripts\activate.bat"
+'''
+
+# Deactivate the virtual environment
+# deactivate
+
+# Output the list of installed packages and their versions to a file
+# pip freeze > requirements.txt
+
+# Install the packages listed in the requirements.txt file
+# pip install -r requirements.txt
