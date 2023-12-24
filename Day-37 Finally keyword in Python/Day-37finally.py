d9df91ec2482702c36970aed5d7a5d1c97657584
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-24 20:53:22 +0530

    Modified Day-37 Finally keyword in Python

--- Day-37 Finally keyword in Python/Day-37finally.py
+++ Day-37 Finally keyword in Python/Day-37finally.py
@@ -1,4 +1,5 @@
 # Day-37 Finally keyword in Python
+
 #Syntax is below in  ''' '''
 '''try:
    #statements which could generate 
@@ -8,7 +9,6 @@ except:
 finally:
     #block of code which is going to 
     #execute in any situation'''
-
 
 # always execute the code that is done by finally keyword
 def func1():
