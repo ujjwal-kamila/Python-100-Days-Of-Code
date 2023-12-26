12a8e1b398e77ca19fa312fdef494a7300fe4126
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-26 20:39:49 +0530

    Day-46 Os module in Python

---
+++ Day-46 Os module in Python/Day46 Os_module.py
@@ -0,0 +1,28 @@
+# Day-46 Os module in Python
+#to create 1-100folders 
+# import os
+# if(not os.path.exists("data")):
+#     os.mkdir("Python") # create a folder name  "Python"
+
+# for i in range (0,100):
+#     os.mkdir(f"Python/Day-{i+1}") #create 1-100folders in python
+
+# to print the folders 
+import os 
+folders = os.listdir("Python")
+print(folders)
+
+#to rename os.rename
+
+#prints indivusal folders
+for folder in folders:
+    print(folder)
+
+#prints folders and their files
+for folder in folders:
+    print(folder)
+    print(os.listdir(f"Python/{folder}"))
+
+
+    
+print(os.getcwd())
