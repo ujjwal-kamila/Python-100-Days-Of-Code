e9f5ce8986d70749291042a3fdcb33e9e64da717
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-27 21:07:07 +0530

    Day-49 File Operations In Python

---
+++ Day-49 File IO (File Handling)  Python/Day-49File_Io.py
@@ -0,0 +1,24 @@
+#Day-49 File Operations In Python
+
+#File operations in Python 
+# Opening a File
+## f = open('myfile.txt', 'r')
+
+# READING A FILE
+
+# f = open('myfile.txt', 'r')
+# # print(f)
+# text = f.read()
+# print(text)
+# f.close()
+
+# WRITING A FILE
+
+f = open('myfile.txt', 'a')
+f.write('Hello, world!')
+#Closing a File
+f.close()
+
+with open('myfile.txt', 'a') as f:
+  f.write("Hey I am inside with")
+  
