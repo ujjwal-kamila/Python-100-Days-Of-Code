a02774527df0c3232e3bd9e2872b9ca3de16cea8
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2023-12-28 13:53:45 +0530

    Day-48 Local and Global variables in Python

--- Day-48 Local and Global variables in Python/day48local_globalvar.py
+++ Day-48 Local and Global variables in Python/day48local_globalvar.py
@@ -27,5 +27,5 @@ def my_function():
 
 
 my_function()
-print(x)  # prints 5
+print(x)   # prints 5
 # print(y) # this will cause an error because y is a local variable and is not accessible outside of the function
