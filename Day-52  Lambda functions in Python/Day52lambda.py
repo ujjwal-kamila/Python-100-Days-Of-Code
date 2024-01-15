ef45017f7c5d1b71c27d6489acff07367a3149d3
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-16 00:25:43 +0530

    Day-52 Lambda functions in Python

--- Day-52  Lambda functions in Python/Day52lambda.py
+++ Day-52  Lambda functions in Python/Day52lambda.py
@@ -20,6 +20,7 @@ print(double(5))
 print(cube(5))
 print(avg(3,7))
 print(avg3(3,5,9))
+
 #we can use "lambda x : x*x*x" instead of cube
 print(apply(cube,4))  #prints 64+6 = 70
 print(apply(double,5))
