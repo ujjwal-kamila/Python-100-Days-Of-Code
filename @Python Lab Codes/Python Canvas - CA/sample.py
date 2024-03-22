402efa9f346ff26e4693924de27f9a48a982fba4
Ujjwal <ujjwalkamila86@gmail.com>
2024-03-22 23:00:13 +0530

    Day-53 Map, Filter & Reduce in Python

---
+++ @Python Lab Codes/Python Canvas - CA/sample.py
@@ -0,0 +1,14 @@
+n = 15
+m = 7
+addition = n + m
+print("Addition:", addition)
+subtraction = n - m
+print("Subtraction:", subtraction)
+multiplication = n * m
+print("Multiplication:", multiplication)
+division = n / m
+print("Division:", division)
+modulus = n % m
+print("Modulus:", modulus)
+floor_division = n // m
+print("Floor Division:", floor_division)
